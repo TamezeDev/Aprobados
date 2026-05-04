494aa862225b6ea9c15f14b0007a78255ee8bf2a
Ezequiel Tamayo <EzequielTamayo@gmail.com>
2026-05-04 17:05:11 +0200

    Añade responsive en vista registro y función para mantener las proporciones de la pantalla del usuario

--- src/main/java/org/zeki/aprobados/controller/StartController.java
+++ src/main/java/org/zeki/aprobados/controller/StartController.java
@@ -3,8 +3,6 @@ package org.zeki.aprobados.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
 import org.zeki.aprobados.helper.SceneHelper;
 
 import java.net.URL;
@@ -18,16 +16,9 @@ public class StartController implements Initializable {
     @FXML
     private Button registerBtn;
 
-    @FXML
-    private ImageView mainImgView;
-
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        initGui();
         actions();
-    }
-
-    private void initGui() {
     }
 
     private void actions() {
@@ -37,12 +28,4 @@ public class StartController implements Initializable {
         registerBtn.setOnAction(event -> SceneHelper.changeScene(registerBtn, AppController.getInstance().getSCENE_PATH().getREGISTER_VIEW()));
 
     }
-
-    private void setResponsiveBackground(){
-
-        HBox parent = (HBox) mainImgView.getParent();
-        mainImgView.fitHeightProperty().bind(parent.heightProperty());
-        mainImgView.fitWidthProperty().bind(parent.widthProperty());
-    }
-
 }
--- src/main/java/org/zeki/aprobados/helper/SceneHelper.java
+++ src/main/java/org/zeki/aprobados/helper/SceneHelper.java
@@ -18,11 +18,25 @@ public class SceneHelper {
             FXMLLoader loader = new FXMLLoader(Main.class.getResource(url));
             Scene scene = new Scene(loader.load());
             Stage stage = (Stage) node.getScene().getWindow();
+            // Keep user size screen
+            setWindowSize(stage);
             stage.setScene(scene);
             stage.show();
 
         } catch (IOException e) {
             throw new CambioEscenaException("Error en la ruta de la escena");
+        }
+    }
+
+    private  static void setWindowSize(Stage stage){
+
+        if (stage.isMaximized()) stage.setMaximized(true);
+        else {
+            double height = stage.getHeight();
+            double width = stage.getWidth();
+
+            stage.setWidth(width);
+            stage.setHeight(height);
         }
     }
 }
